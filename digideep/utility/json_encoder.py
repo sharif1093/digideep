8e43aebf1f7a454d93a0bf1c93c8ef4a3356d939
mrsharif <sharif1093@gmail.com>
2019-08-25 14:51:54 -0400

    Added specific JSON encoder only for numbers.

--- digideep/utility/json_encoder.py
+++ digideep/utility/json_encoder.py
@@ -1,6 +1,10 @@
 import json
 import numpy as np
 
+
+############################################
+## JSON Encoder/Decoder for generic usage ##
+############################################
 class MultiDimensionalArrayEncoder(json.JSONEncoder):
     """
     A ``JSONEncoder`` which ca serialize tuples. See `Stack Overflow`_ post for more information.
@@ -24,7 +28,7 @@ class MultiDimensionalArrayEncoder(json.JSONEncoder):
                 return item
         return super(MultiDimensionalArrayEncoder, self).encode(hint_tuples(obj))
 
-def hinted_tuple_hook(obj):
+def hinted_hook(obj):
     """
     Args:
         obj: ``'{"a":12, "beta":{"__tuple__": True, "items": [0.9, 0.99]}}'``
@@ -39,4 +43,38 @@ def hinted_tuple_hook(obj):
 
 enc = MultiDimensionalArrayEncoder()
 JsonEncoder = lambda obj: enc.encode(obj)
-JsonDecoder = lambda jsonstring: json.loads(jsonstring, object_hook=hinted_tuple_hook)
+JsonDecoder = lambda jsonstring: json.loads(jsonstring, object_hook=hinted_hook)
+
+
+############################################
+## JSON Encoder/Decoder for numbers alone ##
+############################################
+
+class NumArrayEncoder(json.JSONEncoder):
+    """
+    A ``JSONEncoder`` which ca serialize tuples. See `Stack Overflow`_ post for more information.
+
+    .. _Stack Overflow: https://stackoverflow.com/a/15721641
+    """
+    def encode(self, obj):
+        def hint_tuples(item):
+            if isinstance(item, list):
+                return [hint_tuples(e) for e in item]
+            elif isinstance(item, dict):
+                return {key: hint_tuples(value) for key, value in item.items()}
+            elif isinstance(item, np.ndarray):
+                return item.tolist()
+            elif type(item).__module__ == np.__name__:
+                return item.item()
+            else:
+                return item
+        return super(NumArrayEncoder, self).encode(hint_tuples(obj))
+
+def num_decoder(obj):
+    """
+    """
+    return np.array(obj)
+
+enc_num = NumArrayEncoder()
+JsonNumEncoder = lambda obj: enc_num.encode(obj)
+JsonNumDecoder = lambda jsonstring: json.loads(jsonstring, object_hook=num_decoder)
