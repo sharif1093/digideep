421df0a9cec8d53d1389302fc6eb0b39debbd864
mrsharif <sharif1093@gmail.com>
2020-07-06 12:26:19 -0400

    Print cmd and msg. Finalize session.

--- digideep/main.py
+++ digideep/main.py
@@ -1,13 +1,11 @@
-def main():
-    import os, sys
-    import yaml, json
+import os, sys
+import yaml, json
 
-    from digideep.pipeline import Session
-    from digideep.utility.logging import logger
-    from digideep.utility.toolbox import get_class, get_module, strict_update
+from digideep.pipeline import Session
+from digideep.utility.logging import logger
+from digideep.utility.toolbox import get_class, get_module, strict_update
 
-    session = Session(root_path=os.path.dirname(os.path.realpath(__file__)))
-
+def main(session):
     ##########################################
     ### LOOPING ###
     ###############
@@ -23,21 +21,26 @@ def main():
         ParamEngine = get_module(session.args["params"])
         cpanel = strict_update(ParamEngine.cpanel, session.args["cpanel"])
         params = ParamEngine.gen_params(cpanel) ## Generate params from cpanel everytime
-        # print(ParamEngine)
 
-        # Summary
-        # cmd = ' '.join(sys.argv)
-        # logger("Command:", cmd, "\n")
-        # logger.info("Hyper-Parameters\n\n{}".format(yaml.dump(params, indent=2)) )
-        logger.info("Hyper-Parameters\n\n{}".format(json.dumps(cpanel, indent=4, sort_keys=False)) )
         # Storing parameters in the session.
+        params = session.update_params(params)
         session.dump_cpanel(cpanel)
         session.dump_params(params)
+
+        # Summary
+        logger.warn("="*50)
+        logger.warn("Session:", params["session_name"])
+        logger.warn("Message:", params["session_msg"])
+        logger.warn("Command:\n\n$", params["session_cmd"], "\n")
+        logger.warn("-"*50)
+        # logger.info("Hyper-Parameters\n\n{}".format(yaml.dump(params, indent=2)) )
+        logger.warn("Hyper-Parameters\n\n{}".format(json.dumps(cpanel, indent=4, sort_keys=False)) )
+        logger.warn("="*50)
         ##########################################
         
         Runner = get_class(params["runner"]["name"])
         runner = Runner(params)
-    
+
     # 2. Initializing: It will load_state_dicts if we are in loading mode
     runner.start(session)
 
@@ -49,4 +52,10 @@ def main():
     ##########################################
 
 if __name__ == "__main__":
-    main()
+    session = Session(root_path=os.path.dirname(os.path.realpath(__file__)))
+    try:
+        main(session)
+    finally:
+        session.finalize()
+
+    # session.run_sacred()
