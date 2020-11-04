a76afb63f2f1aaa4a2e760e5d2af841facc260e6
mrsharif <sharif1093@gmail.com>
2020-11-03 20:28:12 -0500

    Important update on session

--- digideep/pipeline/session.py
+++ digideep/pipeline/session.py
@@ -1,36 +1,22 @@
-import os, sys, datetime, argparse
+import os, sys, argparse
 import fcntl
 import re
 from shutil import copytree, copyfile, ignore_patterns
 import pipes
-import subprocess
-import threading
-import time
+# import subprocess
+# import threading
+# import time
 
 from digideep.utility.logging import logger
 from digideep.utility.toolbox import dump_dict_as_json, dump_dict_as_yaml, get_module
 from digideep.utility.json_encoder import JsonDecoder
 from digideep.utility.monitoring import monitor
 from digideep.utility.profiling import profiler, KeepTime
-from digideep.utility.name_generator import get_random_name
+from digideep.utility.name_generator import make_unique_path_session
+
 
 import pickle, torch
 from copy import deepcopy
-
-def generateTimestamp():
-    # Always uses UTC as timezone
-    now = datetime.datetime.now()
-    timestamp = '{:%Y%m%d%H%M%S}'.format(now)
-    return timestamp
-
-def make_unique_path_session(path_base_session, prefix="session_"):
-    session_name = prefix + generateTimestamp() + "_" + get_random_name()
-    path_session = os.path.join(path_base_session, session_name)
-    try:
-        os.makedirs(path_session)
-        return path_session
-    except FileExistsError as e:
-        return make_unique_path_session(path_base_session=path_base_session, prefix=prefix)
 
 
 def print_verbose(*args, verbose=False, **kwargs):
@@ -120,28 +106,28 @@ def check_checkpoint(path, verbose=False):
 
 
 
-class ParCommand(threading.Thread):
-    def __init__(self, command, logger):
-        self.stdout = None
-        self.stderr = None
-        self.command = command
-        self.logger = logger
-        threading.Thread.__init__(self)
-    def run(self):
-        t = time.time()
-        p = subprocess.Popen(self.command,
-                             shell=False,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        self.stdout, self.stderr = p.communicate()
-        p.wait()
-        # self.logger.warn(" Thread '", " ".join(self.command), "' is over with exit code = ", p.returncode, " in ",time.time()-t," seconds.", sep="")
-        self.logger.warn("Command: '{}' is over with exit code: {} in {:6.2f} seconds".format(" ".join(self.command),
-                                                                                             p.returncode,
-                                                                                             time.time()-t))
-                        
-        # t.wait()
-        # t.poll()
+# class ParCommand(threading.Thread):
+#     def __init__(self, command, logger):
+#         self.stdout = None
+#         self.stderr = None
+#         self.command = command
+#         self.logger = logger
+#         threading.Thread.__init__(self)
+#     def run(self):
+#         t = time.time()
+#         p = subprocess.Popen(self.command,
+#                              shell=False,
+#                              stdout=subprocess.PIPE,
+#                              stderr=subprocess.PIPE)
+#         self.stdout, self.stderr = p.communicate()
+#         p.wait()
+#         # self.logger.warn(" Thread '", " ".join(self.command), "' is over with exit code = ", p.returncode, " in ",time.time()-t," seconds.", sep="")
+#         self.logger.warn("Command: '{}' is over with exit code: {} in {:6.2f} seconds".format(" ".join(self.command),
+#                                                                                              p.returncode,
+#                                                                                              time.time()-t))
+#
+#         # t.wait()
+#         # t.poll()
 
 
 writers = []
@@ -211,12 +197,13 @@ class Session(object):
         self.is_loading = True if self.args["load_checkpoint"] else False
         self.is_playing = True if self.args["play"] else False
         self.is_resumed = True if self.args["resume"] else False
+        self.is_customs = True if self.args["custom"] else False
         self.is_session_only = True if self.args["create_session_only"] else False
         
-        assert (self.is_loading and self.is_playing) or (self.is_loading and self.is_resumed) or (not self.is_loading), \
-            "--load-checkpoint argument should either be used with --play or --resume arguments."
-        assert (self.is_session_only and (not self.is_loading) and (not self.is_playing) and (not self.is_resumed)) or (not self.is_session_only), \
-            "--create-session-only argument cannot be used with any of the --load-checkpoint, --play, or --resume arguments."
+        assert (self.is_loading and self.is_playing) or (self.is_loading and self.is_resumed) or (self.is_loading and self.is_customs) or (not self.is_loading), \
+            "--load-checkpoint argument should be used either with --play, --resume, or --custom arguments."
+        assert (self.is_session_only and (not self.is_loading) and (not self.is_playing) and (not self.is_resumed) and (not self.is_customs)) or (not self.is_session_only), \
+            "--create-session-only argument cannot be used with any of the --load-checkpoint, --play, --resume, or --custom arguments."
 
         # Automatically find the latest checkpoint if not specified
         self.state['checkpoint_name'] = None
@@ -255,6 +242,11 @@ class Session(object):
             directory = os.path.dirname(os.path.dirname(self.args["load_checkpoint"]))
             self.state['path_base_sessions'] = os.path.split(directory)[0]
             self.args['session_name'] = os.path.split(directory)[1]
+        elif self.is_loading and self.is_customs:
+            # If we are doing a custom task from a checkpoint, we must save the results into the `customs` path
+            # of that session.
+            checkpoint_path = os.path.split(self.args["load_checkpoint"])[0]
+            self.state['path_base_sessions'] = os.path.join(os.path.split(checkpoint_path)[0], "customs")
         else:
             # OK, we are loading from a checkpoint, just create session from scratch.
             # self.state['path_root_session']  = self.args["session_path"]
@@ -266,16 +258,30 @@ class Session(object):
         try: # TODO: and not self.dry_run:
             os.makedirs(self.state['path_base_sessions'])
             # Create an empty __init__.py in it!
-            with open(os.path.join(self.state['path_base_sessions'], '__init__.py'), 'w') as f:
-                print("", file=f)
+        except FileExistsError:
+            pass
         except Exception as ex:
             print(ex)
+        
+        try:
+            with open(os.path.join(self.state['path_base_sessions'], '__init__.py'), 'w') as f:
+                print("", file=f)
+        except FileExistsError:
+            pass
+        except Exception as ex:
+            print(ex)
+
 
         # 2. Create a unique 'path_session':
         if not self.dry_run:
             if self.args['session_name']:
                 # If is_loading then this line will be executed ...
                 self.state['path_session'] = os.path.join(self.state['path_base_sessions'], self.args["session_name"])
+                # TODO: Make the directory
+                try:
+                    os.makedirs(self.state['path_session'])
+                except Exception as ex:
+                    print(ex)
             else:
                 self.state['path_session'] = make_unique_path_session(self.state['path_base_sessions'], prefix="session_")
         else:
@@ -291,19 +297,23 @@ class Session(object):
         self.state['path_tensorboard'] = os.path.join(self.state['path_session'], 'tensorboard')
         # Hyper-parameters basically is a snapshot of intial parameter engine's state.
         self.state['file_cpanel'] = os.path.join(self.state['path_session'], 'cpanel.json')
+        self.state['file_repeal'] = os.path.join(self.state['path_session'], 'repeal.json')
         self.state['file_params'] = os.path.join(self.state['path_session'], 'params.yaml')
+
         self.state['file_report'] = os.path.join(self.state['path_session'], 'report.log')
         # self.state['file_visdom'] = os.path.join(self.state['path_session'], 'visdom.log')
         self.state['file_varlog'] = os.path.join(self.state['path_session'], 'varlog.json')
         self.state['file_prolog'] = os.path.join(self.state['path_session'], 'prolog.json')
         self.state['file_monlog'] = os.path.join(self.state['path_session'], 'monlog.json')
         self.state['lock_running'] = os.path.join(self.state['path_session'], 'running.lock')
+        self.state['lock_done']    = os.path.join(self.state['path_session'], 'done.lock')
 
         # Here, the session path has been created or it existed.
         # Now make sure only one instance passes from this point.
         self.check_singleton_instance()
+        self.check_if_done()
 
-        
+
         # 3. Creating the rest of paths:
         if not self.is_playing and not self.is_resumed and not self.dry_run:
             os.makedirs(self.state['path_checkpoints'])
@@ -311,7 +321,7 @@ class Session(object):
         if not self.is_resumed and not self.dry_run:
             os.makedirs(self.state['path_monitor'])
         
-        
+
         self.initLogger()
         self.initVarlog()
         self.initProlog()
@@ -453,6 +463,10 @@ class Session(object):
         if self.dry_run:
             return
         dump_dict_as_json(self.state['file_cpanel'], cpanel)
+    def dump_repeal(self, repeal):
+        if self.dry_run:
+            return
+        dump_dict_as_json(self.state['file_repeal'], repeal)
     def dump_params(self, params):
         if self.dry_run:
             return
@@ -489,7 +503,7 @@ class Session(object):
     ## Locks: done.lock, running.lock ##
     ####################################
     def check_singleton_instance(self):
-        if self.is_playing:
+        if self.is_playing or self.is_customs:
             return
         if not self.is_loading:
             # Create running.lock for the first time
@@ -501,10 +515,22 @@ class Session(object):
             fcntl.lockf(lock_file_pointer, fcntl.LOCK_EX | fcntl.LOCK_NB)
         except IOError:
             print("ERROR: Another instance is already running. Make sure to exit from the other one to continue.", file=sys.stderr)
+            print("       Currently running {}".format(self.state['path_session']), file=sys.stderr)
             sys.exit(2)
 
+    def check_if_done(self):
+        if self.is_playing or self.is_customs:
+            return
+        if not self.is_loading:
+            return
+        if os.path.isfile(self.state['lock_done']):
+            print("ERROR: This instance is already done.", file=sys.stderr)
+            print("       Currently running {}".format(self.state['path_session']), file=sys.stderr)
+            sys.exit(3)
+
     def mark_as_done(self):
-        with open(os.path.join(self.state['path_session'], 'done.lock'), 'w') as f:
+        # os.path.join(self.state['path_session'], 'done.lock')
+        with open(self.state['lock_done'], 'w') as f:
             print("", file=f)
 
 
@@ -583,6 +609,7 @@ class Session(object):
             os.makedirs(dirname)
         filename = os.path.join(dirname, "runner.pt")
         pickle.dump(runner, open(filename, "wb"), pickle.HIGHEST_PROTOCOL)
+        print(">> 5")
         logger.warn('>>> Network runner saved at {}\n'.format(dirname))
     #################################
 
@@ -601,6 +628,7 @@ class Session(object):
         parser.add_argument('--load-checkpoint', metavar=('<path>'), default='', type=str, help="Load a checkpoint to resume training from that point.")
         parser.add_argument('--play', action="store_true", help="Will play the stored policy.")
         parser.add_argument('--resume', action="store_true", help="Will resume training the stored policy.")
+        parser.add_argument('--custom', action="store_true", help="Will load a checkpoint and run a custom runner.")
         parser.add_argument('--create-session-only', action="store_true", help="If used, only the barebone session will be created and nt training/evaluation/loading will happen.")
         parser.add_argument('--dry-run', action="store_true", help="If used, no footprints will be stored on disc whatsoever.")
         ## Session
@@ -624,6 +652,7 @@ class Session(object):
         parser.add_argument('--params', metavar=('<name>'), default='', type=str, help="Choose the parameter set.")
         parser.add_argument('--cpanel', metavar=('<json dictionary>'), default=r'{}', type=JsonDecoder, help="Set the parameters of the cpanel by a json dictionary.")
 
+        parser.add_argument('--repeal', metavar=('<json dictionary>'), default=r'{}', type=JsonDecoder, help="Set parameter values to be overridden.")
         # NOTE: No default value for params. MUST be specified explicitly. "digideep.params.mujoco"
         ##
         # parser.add_argument('--override', action='store_true', help="Provide this option to explicitly override saved options with new options.")
