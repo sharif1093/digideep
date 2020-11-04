e899e016df1182ef703cbb6a54b64f809b418a3f
mrsharif <sharif1093@gmail.com>
2020-11-03 20:22:08 -0500

    Updated

--- digideep/saam.py
+++ digideep/saam.py
@@ -60,13 +60,13 @@ class Loader:
         return os.path.join(self.root_path, "prolog.json")
     def getPrologLoader(self):
         return LogLoader(self._getPrologPath)
-    
+
     @property
     def _getMonlogPath(self):
         return os.path.join(self.root_path, "monlog.json")
     def getMonlogLoader(self):
         return LogLoader(self._getMonlogPath)
-        
+
         
     
     # NOTE: What is this used for?
